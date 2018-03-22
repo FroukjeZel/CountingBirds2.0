90b0117bbf8dcd9e0fd70d2cd4554e2f54cf24b4
Unknown <jsparidaans@gmail.com>
2018-03-22 14:37:22 +0100

    Vrijwilliger en Kalender Activities toegevoegd

---
+++ app/src/main/java/com/example/froukje/countingbirds/Vrijwilliger.java
@@ -0,0 +1,66 @@
+package com.example.froukje.countingbirds;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Vrijwilliger extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vrijwilliger);
+
+
+        String[] vrijw = new String[] {"Joey Sparidaans", "Thierry Janson", "Froukje Zeldenrust",
+                "Mark Schevers"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, vrijw);
+        ListView listView = (ListView) findViewById(R.id.lijst);
+        listView.setAdapter(adapter);
+
+
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.vrijwilliger, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if(id == R.id.action_calendar){
+            Intent intent = new Intent(this, Kalender.class);
+            this.startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
